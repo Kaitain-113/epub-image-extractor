855673d9a354397c29bb9ff69bfb8b78ea7ab151
kaetaen <kaetaen@tutanota.com>
2025-04-03 23:49:15 -0300

    feat: refactor EPUB image extraction to use EpubService and improve file handling

--- epub-image-extractor/main.py
+++ epub-image-extractor/main.py
@@ -1,16 +1,10 @@
-import logging
-import os
-import uuid
-import zipfile
-
-from fastapi import FastAPI, Request, UploadFile, Form 
-from fastapi.responses import HTMLResponse, FileResponse
+import uvicorn
+from fastapi import FastAPI, Form, Request, UploadFile
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import uvicorn
 
-import ebooklib
-from ebooklib import epub
+from .services import EpubService
 
 app = FastAPI()
 
@@ -18,69 +12,28 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 
 templates = Jinja2Templates(directory='templates')
 
-def get_image_from_ebook(book_metadata: dict):
-    book_images = []
-    
-    book = epub.read_epub(book_metadata.get('book_dir'))
-    folder_name = str(uuid.uuid4())
-    output_dir = f"/tmp/epub_image_extractor/extracted_images/{folder_name}/"
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir, exist_ok=True)
-    
-    # TODO: Add a check to see if the book is empty or not
-    for image in book.get_items_of_type(ebooklib.ITEM_IMAGE):
-        image_name: str = image.get_name().split('/').pop()
-        image_path: str = os.path.join(output_dir, image_name)
-
-        with open(image_path, "wb") as img_file:
-            img_file.write(image.get_content())
-
-        book_images.append(image_path)
-        logging.debug(f"Image saved: {image_path}")
-
-    # Create a ZIP file containing all extracted images
-    zip_file_name = book_metadata.get('book_name')
-    zip_file_path = f"/tmp/epub_image_extractor/extracted_images/{folder_name}/{zip_file_name}.zip"
-    with zipfile.ZipFile(zip_file_path, 'w') as zipf:
-        for image_path in book_images:
-            zipf.write(image_path, os.path.basename(image_path))
-            logging.debug(f"Image added to ZIP: {image_path}")
-    #################
-
-    logging.info(f"ZIP file created: {zip_file_path}")
-
-    return zip_file_path
 
 @app.get('/', response_class=HTMLResponse)
 async def render_upload_page(request: Request):
-    return templates.TemplateResponse(request=request, name='upload.jinja', status_code=200)
+    return templates.TemplateResponse(
+        request=request, name='upload.jinja', status_code=200
+    )
+
 
 @app.post('/upload')
 async def extract_image_from_book(epub_file: UploadFile = Form(...)):
-    # Create folders
-    book_images_dir = "/tmp/epub_image_extractor/extracted_images/"
-    book_dir = "/tmp/epub_image_extractor/books/"
+    epub_service = EpubService()
+    await epub_service.save_ebook_file(epub_file)
+    book_images_data: dict = epub_service.get_image_from_ebook(
+        compressed_file_extension='zip'
+    )
 
-    if not os.path.exists(book_images_dir) and not os.path.exists(book_dir):
-        os.makedirs(book_images_dir, exist_ok=True)
-    
-    if not os.path.exists(book_dir):  
-        os.makedirs(book_dir, exist_ok=True)
-    ####################
-    
-    book_metadata = {
-        "book_dir": f"/tmp/epub_image_extractor/books/{epub_file.filename[:20] + str(uuid.uuid4())}",
-        "book_name": epub_file.filename
-    }
-    
-    with open(book_metadata.get("book_dir"), "wb") as file:
-        file.write(await epub_file.read())
-    
-    zip_file = get_image_from_ebook(book_metadata)
-    
-    return FileResponse(zip_file, media_type='application/zip', filename=f"{book_metadata.get('book_name')}.zip")
+    return FileResponse(
+        book_images_data.get('compressed_file_path'),
+        filename=book_images_data.get('file_name'),
+        media_type=book_images_data.get('file_type_header'),
+    )
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8001)
--- epub-image-extractor/services.py
+++ epub-image-extractor/services.py
@@ -1,40 +1,82 @@
 import os
+import zipfile
 
+from ebooklib import ITEM_IMAGE, epub
 from fastapi import UploadFile
 
 from .helpers import generate_uuid_string
 
 
 class EpubService:
-    __BOOK_IMAGES_DIR = "/tmp/epub_image_extractor/extracted_images/"
-    __BOOK_DIR = "/tmp/epub_image_extractor/books/"
+    __BOOK_IMAGES_DIR = 'storage/extracted_images/'
+    __BOOK_DIR = 'storage/books/'
+    __COMPRESSED_FILE_TYPE_HEADER = {
+        'zip': 'application/zip',
+    }
 
     def __init__(self):
         EpubService.__create_storage_folders()
-        self.book_storaged_file_name: str = ""
-        self.book_original_file_name: str = ""
+        self.book_storaged_file_path: str = ''
+        self.book_original_file_name: str = ''
+        self.output_images_dir_name: str = generate_uuid_string()
 
     @classmethod
-    def __create_storage_folders(cls):
+    def __create_storage_folders(cls) -> None:
         """
         Create necessary directories for storing EPUB files and extracted images.
         """
         if not os.path.exists(cls.__BOOK_IMAGES_DIR):
             os.makedirs(cls.__BOOK_IMAGES_DIR, exist_ok=True)
-    
-        if not os.path.exists(cls.__BOOK_DIR):  
+
+        if not os.path.exists(cls.__BOOK_DIR):
             os.makedirs(cls.__BOOK_DIR, exist_ok=True)
 
-    
-
-    async def save_ebook_file(self, epub_file: UploadFile):
+    async def save_ebook_file(self, epub_file: UploadFile) -> None:
         """
         Save the uploaded EPUB file to a temporary directory.
         """
-        book_storage_folder = os.path.join(
-            EpubService.__BOOK_DIR,
-            generate_uuid_string()
+        self.book_original_file_name = epub_file.filename
+        self.book_storaged_file_path = os.path.join(
+            EpubService.__BOOK_DIR, generate_uuid_string(self.book_original_file_name)
         )
 
-        with open(book_storage_folder, "wb") as file:
+        with open(self.book_storaged_file_path, 'wb') as file:
             file.write(await epub_file.read())
+
+    def get_image_from_ebook(self, compressed_file_extension='zip') -> dict[str, str]:
+        book_images = []
+        compressed_file_name = (
+            f'{self.book_original_file_name}.{compressed_file_extension}'
+        )
+        compressed_file_type_header = EpubService.__COMPRESSED_FILE_TYPE_HEADER.get(
+            compressed_file_extension
+        )
+        images_dir = os.path.join(
+            EpubService.__BOOK_IMAGES_DIR, self.output_images_dir_name
+        )
+
+        book = epub.read_epub(self.book_storaged_file_path)
+
+        if not os.path.exists(images_dir):
+            os.makedirs(images_dir, exist_ok=True)
+
+        for image in book.get_items_of_type(ITEM_IMAGE):
+            image_name: str = image.get_name().split('/').pop()
+            image_path: str = os.path.join(images_dir, image_name)
+
+            with open(image_path, 'wb') as img_file:
+                img_file.write(image.get_content())
+
+            book_images.append(image_path)
+
+        compressed_file_path = f'{images_dir}/{compressed_file_name}'
+
+        with zipfile.ZipFile(compressed_file_path, 'w') as zipf:
+            for image_path in book_images:
+                zipf.write(image_path, os.path.basename(image_path))
+
+        return {
+            'file_name': compressed_file_name,
+            'compressed_file_path': compressed_file_path,
+            'filetype_header': compressed_file_type_header,
+        }
